a8850a4be3e9dde9e508a6d227dd30225123cb8a
Fayez O Saadi <fayezosaadi@gmail.com>
2017-04-06 20:41:30 +0000

    Pythagorean Theorem Fayez Saa

--- PythagoreanTheorem.js
+++ PythagoreanTheorem.js
@@ -4,47 +4,34 @@ var input = [
   { x: 8, y: 15 }
 ];
 
-// const pythagorean = ({x, y}) => Math.sqrt((x*x) + (y*y));
-
-var result = input.map(pythagorean);
-// console.log('result', result);
-
-console.log(result[0] === 5);
-console.log(result[1] === 13);
-console.log(result[2] === 17);
 
 
-function pythagorean ({x, y}) {
-  // console.log(x)
-  // console.log(y)
+  // const pythagorean = ({x, y}) => Math.sqrt((x*x) + (y*y));       This is my function in a sigle line of code
 
-  return Math.sqrt((x*x) + (y*y));
-  // console.log(z);
-  // return z; }
-}
+  // function pythagorean ({x, y}) {                                 This is my function in diff version
+  //   return Math.sqrt((x*x) + (y*y));
+  // }
 
 
-// function pythagorean (obj) {
-//   const x = obj.x;
-//   const y = obj.y;
-//   const z = Math.sqrt((x*x) + (y*y));
-//   // console.log(x)
-//   // console.log(y)
-
-//   return z;
-//   // console.log(z);
-//   // return z; }
-// }
+  var result = input.map(pythagorean);
 
 
-  // Object.key(obj).forEach(function(e){
-  //   var z = Math.sqrt(e.x*e.x+e.y+e.y);
-  //   console.log(z);
-  //   return z;
-  // });
-  // var z = Math.sqrt(x*x + y*y);
-  // return z,
-// }
+  console.log(result[0] === 5);
+  console.log(result[1] === 13);
+  console.log(result[2] === 17);
+
+
+
+
+  function pythagorean (obj) {
+    const x = obj.x;
+    const y = obj.y;
+    const z = Math.sqrt((x*x) + (y*y));
+    return z;
+  }
+
+
+
 
 
 
